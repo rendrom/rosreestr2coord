afa53dbed3d912ddce5f67161e5b338bed36b63f
Niakr1s <pavel2188@gmail.com>
2018-03-04 20:16:45 +0300

    reverted to original

--- rosreestr2coord.py
+++ rosreestr2coord.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
 from scripts.console import main
-from scripts.parser import Area
 if __name__ == "__main__":
     main()
-    area = Area('21:01:010305:101')
-    print(area.to_geojson_poly())
