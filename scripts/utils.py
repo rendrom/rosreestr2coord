06a605434a454a8db7e54200a675a94bc2601bd1
Niakr1s <pavel2188@gmail.com>
2018-03-04 20:05:30 +0300

    Прикрутил прокси.
    Если в корневой папке нет файла proxy.txt, или в 1 его строке прописано None - то работает без прокси.
    Если файл proxy.txt существует, будет работать через прокси и отбраковывать неработающие. Если файл станет пустым - скачает новые прокси c http://www.ip-adress.com/proxy_list/

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,6 @@
 from __future__ import division
+import proxy_handling
+import urllib2
 
 # Try to send request through a TOR
 # try:
@@ -71,15 +73,51 @@ class TimeoutException(Exception):
 
 
 def make_request(url):
-    import urllib2
+    # original function
     if url:
         url = url.encode('utf-8')
         logger.debug(url)
-        try:
-            f = urllib2.urlopen(url)
-            read = f.read()
-            return read
-        except Exception as er:
-            logger.warning(er)
-            raise TimeoutException()
+        proxies = proxy_handling.load_proxies_from_file()
+        if proxies == None or (len(proxies) != 0 and proxies[0] == 'None'):
+            try:
+                f = urllib2.urlopen(url)
+                read = f.read()
+                return read
+            except Exception as er:
+                logger.warning(er)
+                raise TimeoutException()
+        else:
+            return make_request_with_proxy(url)
+    return False
+
+
+def make_request_with_proxy(url):
+    while True:
+        proxies = proxy_handling.load_proxies_from_file()
+        if not proxies:
+            proxy_handling.update_proxies()
+            proxies = proxy_handling.load_proxies_from_file()
+        removed = False
+        tries = 3  # number of tries for each proxy
+        for proxy in proxies:
+            for i in range(1, tries+1):  # how many tries for each proxy
+                try:
+                    print('%i iteration of proxy %s' % (i, proxy))
+                    proxy_handler = urllib2.ProxyHandler({'http': proxy, 'https': proxy})
+                    opener = urllib2.build_opener(proxy_handler)
+                    urllib2.install_opener(opener)
+                    headers = {
+                        'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+                        'referer': 'htpps://www.google.com/'}
+                    request = urllib2.Request(url, headers=headers)
+                    f = urllib2.urlopen(request)
+                    read = f.read()
+                    return read
+                except Exception as er:
+                    logger.warning(er)
+                if i == tries:
+                    proxies.remove(proxy)
+                    removed = True
+        if removed:
+            proxy_handling.dump_proxies_to_file(proxies)
     return False
