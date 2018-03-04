6fb423754d68631b5eaf9b7d7a0b2de827c82fc7
Niakr1s <pavel2188@gmail.com>
2018-03-04 20:18:49 +0300

    cleaned module

--- scripts/proxy_handling.py
+++ scripts/proxy_handling.py
@@ -34,12 +34,6 @@ def download_proxies(path='proxy.txt'):
     dump_proxies_to_file(found[:20], path)  # 20 top proxies
 
 
-def get_random_proxy(path='proxy.txt'):
-    with open(path) as outfile:
-        proxies = outfile.readlines()
-    return choice(proxies)
-
-
 def load_proxies_from_file(path='proxy.txt'):
     try:
         with open(path) as outfile:
@@ -52,20 +46,3 @@ def dump_proxies_to_file(proxies, path='proxy.txt'):
     with open(path, 'w') as outfile:
         for proxy in proxies:
             outfile.write(proxy)
-
-
-if __name__ == '__main__':
-    # rand_proxy = '37.187.116.100:801'
-    # proxy = urllib2.ProxyHandler({'http': rand_proxy, 'https': rand_proxy})
-    # opener = urllib2.build_opener(proxy)
-    # urllib2.install_opener(opener)
-    # headers = {
-    #     'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
-    #     'referer': 'htpps://www.google.com/'}
-    # request = urllib2.Request('https://2ip.ru/', headers=headers)
-    # urlopen = urllib2.urlopen(request)
-    # with open('temp.htm', 'w') as file:
-    #     file.write(urlopen.read())
-    # print(load_proxies_from_file())
-    # download_proxies()
-    update_proxies()
