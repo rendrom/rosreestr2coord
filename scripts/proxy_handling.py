d1fd989f82a8acfa5e53bb7f13d6b95afe4013ce
Konstantin Goncharik <botankras@gmail.com>
2019-10-16 14:00:45 +0700

    Switch proxies downloading to https protocol

--- scripts/proxy_handling.py
+++ scripts/proxy_handling.py
@@ -25,10 +25,10 @@ def download_proxies(path=PROXY_PATH):
     # Downloading without proxy
     opener = urllib2.build_opener(urllib2.ProxyHandler())
     urllib2.install_opener(opener)
-    request = urllib2.Request('http://www.ip-adress.com/proxy_list/')
+    request = urllib2.Request('https://www.ip-adress.com/proxy_list/')
     request.add_header('user-agent',
                        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36')
-    request.add_header('referer', 'htpps://www.google.com/')
+    request.add_header('referer', 'https://www.ip-adress.com/')
     f = urllib2.urlopen(request)
     pattern = r'\d*\.\d*\.\d*\.\d*\</a>:\d*'
     found = [i.replace('</a>', '') + '\n' for i in re.findall(pattern, f.read())]
