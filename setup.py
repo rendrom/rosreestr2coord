dcf36ab173f8b521e257886e96d7c55a5bcd578b
Artemiy <rendrom@gmail.com>
2022-06-24 11:21:40 +0200

    chore: bump dependencies

--- setup.py
+++ setup.py
@@ -12,29 +12,13 @@ setup(
     include_package_data=True,
     license="MIT",
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         "Development Status :: 5 - Production/Stable",
-        # Indicate who your project is intended for
-        # 'Intended Audience :: Developers',
-        # 'Topic :: Software Development :: Build Tools',
-        # Pick your license as you wish (should match "license" above)
         "License :: OSI Approved :: MIT License",
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        # 'Programming Language :: Python :: 2',
-        # 'Programming Language :: Python :: 2.6',
-        # 'Programming Language :: Python :: 2.7',
         "Programming Language :: Python :: 3",
-        # 'Programming Language :: Python :: 3.3',
-        # 'Programming Language :: Python :: 3.4',
-        # 'Programming Language :: Python :: 3.5',
     ],
     description="Get geometry from rosreestr",
     long_description="Get area coordinates by its cadastral number",
-    install_requires=["numpy", "Pillow>=8.2", "opencv-python"],
+    install_requires=["numpy==1.23.0", "Pillow==9.1.1", "opencv-python==4.6.0.*"],
     url="https://github.com/rendrom/rosreestr2coord",
     author="Artemiy Doroshkov",
     author_email="rendrom@gmail.com",
